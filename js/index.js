53ac3dece94091912558b4b6d215bed27e7bd9b2
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-13 18:44:02 -0500

    Add and remove toDos.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 var toDoApp = new Vue({
   el: '#toDoApp',
   data: {
-    message: 'hey...'
+    title: 'My ToDo´s',
+    toDos: [
+      {description: "Follow @MiniCoding", done: false},
+      {description: "RT this", done: false},
+      {description: "Check our codes on GitHub", done: false}
+    ],
+    newToDo: ''
+  },
+  methods: {
+    add: function(){
+      if(this.newToDo){
+        this.toDos.push({description: this.newToDo, done: false})
+        this.newToDo = ''
+      }
+    },
+    remove: function(element){
+      this.toDos.splice(element,1)
+    }
   }
 })
